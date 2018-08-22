83087d3108e8a996fdcdb96b96eac077afa1a16c
elzup <guild0105@gmail.com>
2018-08-22 13:55:17 +0900

    Top styles

--- src/containers/TopPage/index.js
+++ src/containers/TopPage/index.js
@@ -1,16 +1,58 @@
 // @flow
 import * as React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
 import type { State as RootState } from '../../types'
 // import * as selectors from './selectors'
+
+const meta = {
+	w: 650,
+	h: 650,
+}
+
+const MainFrame = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	min-width: ${meta.w}px;
+	min-height: ${meta.h}px;
+`
+
+const TopScreen = styled.div`
+	position: absolute;
+	left: calc(50% - ${meta.w / 2}px);
+	top: calc(50% - ${meta.h / 2}px);
+	width: ${meta.w}px;
+	height: ${meta.h}px;
+	background-color: #aaa;
+	overflow: hidden;
+`
+
+const SecondScreen = styled.div`
+	position: absolute;
+	left: calc(50% - ${meta.w / 2}px);
+	top: 100%;
+	width: ${meta.w}px;
+	background-color: orange;
+	overflow: hidden;
+`
 
 type Props = {}
 
 const C = (props: Props) => {
 	return (
-		<div>
-			<p>hoge</p>
-		</div>
+		<MainFrame>
+			<TopScreen>
+				<h1>another node</h1>
+				<section>about</section>
+				<section>products</section>
+				<section>arts</section>
+			</TopScreen>
+			<SecondScreen>hoge</SecondScreen>
+		</MainFrame>
 	)
 }
 
