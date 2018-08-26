2896b875961047c410abb9ce6a7d43c9a6f963d8
elzup <guild0105@gmail.com>
2018-08-26 23:13:17 +0900

    refactor

--- src/components/HobbyScreen/index.js
+++ src/components/HobbyScreen/index.js
@@ -18,48 +18,87 @@ const Name = styled.th`
 	border-right: solid;
 `
 
+type Link = {
+	href: string,
+	text: string,
+}
+
+type Category = {
+	category: string,
+	description: string,
+	links: Link[],
+}
+
+const hobbies: Category[] = [
+	{
+		category: 'ゲーム',
+		description: 'Minecraft, Splatoon2, VRChat, ネクロダンサー',
+		links: [
+			{
+				href: 'https://elzup.tumblr.com/tagged/minecraft',
+				text: 'Minecraft作品',
+			},
+			{
+				href: 'https://anozon10.tumblr.com/tagged/game',
+				text: 'ゲーム実績',
+			},
+		],
+	},
+	{
+		category: 'パズル',
+		description: 'ルービックキューブ, パズルゲーム, ペンシルパズル',
+		links: [
+			{
+				href: "http://elzup.tumblr.com/tagged/rubick's%20cube",
+				text: 'パターンキューブ作品',
+			},
+		],
+	},
+	{
+		category: 'ボドゲ',
+		description: 'オセロ, 将棋',
+		links: [
+			{
+				href: 'https://shogiwars.heroz.jp/users/elzup',
+				text: '将棋ウォーズ3級',
+			},
+		],
+	},
+	{
+		category: 'アニメ',
+		description: 'コードギアス、進撃の巨人、亜人、キノの旅',
+		links: [
+			{
+				href: 'https://annict.jp/@elzup/watched',
+				text: '見たアニメ',
+			},
+			{
+				href: 'https://annict.jp/@elzup/TODO',
+				text: '好きなアニメ',
+			},
+			{
+				href: 'https://elzup.tumblr.com/tagged/yome',
+				text: '嫁',
+			},
+		],
+	},
+]
+
 const HobbyScreen = (props: Props) => (
 	<Screen id="hobby" title="Hobby" description="趣味">
 		<div>
 			<Table>
-				<tr>
-					<Name>ゲーム</Name>
-					<td>Minecraft, Splatoon2</td>
-					<td>
-						<LinkButton
-							href="https://elzup.tumblr.com/tagged/minecraft"
-							text="Minecraft作品"
-						/>
-						<LinkButton
-							href="https://anozon10.tumblr.com/tagged/game"
-							text="ゲーム実績"
-						/>
-					</td>
-				</tr>
-				<tr>
-					<Name>パズル</Name>
-					<td>ルービックキューブ, パズルゲーム, ペンシルパズル</td>
-					<td />
-				</tr>
-				<tr>
-					<Name>ボドゲ</Name>
-					<td>オセロ, 将棋</td>
-					<td />
-				</tr>
-				<tr>
-					<Name>アニメ</Name>
-					<td>コードギアス、進撃の巨人、亜人、キノの旅</td>
-					<td>
-						<LinkButton
-							href="https://annict.jp/@elzup/watched"
-							text="見たアニメ"
-						/>
-						<LinkButton
-							href="https://annict.jp/@elzup/TODO"
-							text="好きなアニメ"
-						/>
-					</td>
-				</tr>
+				{hobbies.map(hobby => (
+					<tr>
+						<Name>{hobby.category}</Name>
+						<td>{hobby.description}</td>
+						<td>
+							{hobby.links.map(link => (
+								<LinkButton href={link.href} text={link.text} />
+							))}
+						</td>
+					</tr>
+				))}
 			</Table>
 		</div>
 	</Screen>
