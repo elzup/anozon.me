d5ee49e4b1e06c685dcc72cb6c8ab5b4cd5f8179
elzup <guild0105@gmail.com>
2019-03-16 00:37:17 +0900

    update: footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,33 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 
 type Props = {}
 
 const Panel = styled.div`
-	height: 50vh;
+	height: 10em;
 	width: 100%;
 	background: black;
 	display: flex;
 	justify-content: center;
 `
-const Another = styled.div`
-	line-height: 50vh;
-	color: transparent;
-	font-size: 25vw;
-	font-weight: 900;
-	text-shadow: 0 0 200px white;
-	transform: rotate(180deg);
+
+const Contents = styled.div`
+	display: flex;
+	width: 100%;
+	padding: 8px;
+	align-items: flex-end;
+	justify-content: flex-end;
+`
+
+const CopyRight = styled.h4`
+	color: white;
 `
 
 const Footer = (props: Props) => (
 	<Panel>
-		<Another />
+		<Contents>
+			<CopyRight>© 2019 anozon</CopyRight>
+		</Contents>
 	</Panel>
 )
 
