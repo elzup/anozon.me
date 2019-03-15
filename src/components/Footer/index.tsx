30aae43ab2ef46c79fe25ac32000cfaa8e2e1782
elzup <guild0105@gmail.com>
2019-03-16 01:00:29 +0900

    Fotter doodle

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import styled, { withTheme } from 'styled-components'
+import { cssDoodle } from '../../utils'
 
 type Props = {}
 
@@ -14,7 +15,8 @@ const Panel = styled.div`
 const Contents = styled.div`
 	display: flex;
 	width: 100%;
-	padding: 8px;
+	z-index: 10;
+	padding: 8px 16px;
 	align-items: flex-end;
 	justify-content: flex-end;
 `
@@ -23,8 +25,37 @@ const CopyRight = styled.h4`
 	color: white;
 `
 
+const DoodleBackground = cssDoodle`
+  :doodle {
+		@grid: 5x9;
+		width: 100%;
+		height: 10em;
+		z-index: 1;
+		position: absolute;
+	}
+
+	color: #ddd;
+	margin: 4px;
+
+
+	--d: 5;
+	--r: 3;
+	--num: calc(
+		@abs(@abs(@row() - 3) + @abs(@col() - 3) - 5) / 5
+	);
+	
+	background: hsla(
+		0, 0%, 100%, calc(var(--num) / 5)
+	);
+
+  :hover { 
+		transform: rotate(90deg);
+	}
+`
+
 const Footer = (props: Props) => (
 	<Panel>
+		<DoodleBackground style={{}} />
 		<Contents>
 			<CopyRight>© 2019 anozon</CopyRight>
 		</Contents>
