521058ca1aa96a344f32558c9d39904c228cceb8
elzup <guild0105@gmail.com>
2020-01-22 05:56:02 +0900

    update: meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,34 @@
 import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
-import { NextSeo } from 'next-seo'
 
 import { GA_TRACKING_ID } from '../src/utils/gtag'
+
+const config = {
+	url: 'https://anozon.me',
+	title: "anozon's site",
+	description: 'about anozon. web programmer, my hobbies: game, puzzle.',
+	image: 'https://anozon.me/static/another-node.png',
+	twitter: '@anozon',
+}
 
 export default class extends Document {
 	render() {
 		return (
 			<html lang="ja">
-				<NextSeo
-					title="anozon's site"
-					description="about anozon. web programmer, my hobbies: game, puzzle."
-					openGraph={{
-						url: 'https://anozon.me',
-						images: [
-							{
-								url: 'https://anozon.me/static/another-node.png',
-								alt: 'anozon site',
-							},
-						],
-						// eslint-disable-next-line @typescript-eslint/camelcase
-						site_name: "anozon's site",
-					}}
-					twitter={{
-						handle: '@anozon',
-						site: '@anozon',
-						cardType: 'summary_large_image',
-					}}
-				/>
 				<Head>
+					<title>{config.title}</title>
+					<link rel="shortcut icon" href="icon.png" />
+					<link rel="icon" type="image/png" href="icon.png" sizes="192x192" />
+
+					<meta name="description" content={config.description} />
+					<meta property="og:url" content={config.url} />
+					<meta property="og:type" content="website" />
+					<meta property="og:title" content={config.title} />
+					<meta property="og:description" content={config.description} />
+					<meta property="og:site_name" content={config.title} />
+					<meta property="og:image" content={config.image} />
+					<meta name="twitter:card" content="summary" />
+					<meta name="twitter:site" content="@anozon" />
 					<meta name="theme-color" content="#eee" />
 					<meta
 						name="viewport"
