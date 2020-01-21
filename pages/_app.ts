640e1abe303200c196aea2a126eb31599f99e667
elzup <guild0105@gmail.com>
2020-01-22 05:40:05 +0900

    fix: next-seo config

--- pages/_app.ts
+++
@@ -1,8 +0,0 @@
-import App from 'next/app'
-import Router from 'next/router'
-
-import * as gtag from '../src/utils/gtag'
-
-Router.events.on('routeChangeComplete', url => gtag.pageview(url))
-
-export default App
