be13c5f96883cfa8046c1104138a269caf43d344
elzup <guild0105@gmail.com>
2020-01-21 21:18:46 +0900

    fix: gtag code

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -4,6 +4,7 @@ export const GA_TRACKING_ID = 'ua-49286104-9'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
+	// @ts-ignore
 	window.gtag('config', GA_TRACKING_ID, {
 		page_path: url,
 	})
@@ -11,6 +12,7 @@ export const pageview = url => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
+	// @ts-ignore
 	window.gtag('event', action, {
 		event_category: category,
 		event_label: label,
