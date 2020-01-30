cce87f72eeb3c94532b863892f379910d34e10d9
elzup <guild0105@gmail.com>
2020-01-30 17:46:18 +0900

    switch to preact

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
+import React, { ElementType } from 'react'
 
 import { GA_TRACKING_ID } from '../src/utils/gtag'
 
@@ -11,36 +10,38 @@ const config = {
 	twitter: '@anozon',
 }
 
-export default class extends Document {
-	render() {
-		return (
-			<html lang="ja">
-				<Head>
-					<title>{config.title}</title>
-					<link rel="shortcut icon" href="icon.png" />
-					<link rel="icon" type="image/png" href="icon.png" sizes="192x192" />
+type DocumentProps = {
+	Main: ElementType
+	Head: ElementType
+	NextScript: ElementType
+}
 
-					<meta name="description" content={config.description} />
-					<meta property="og:url" content={config.url} />
-					<meta property="og:type" content="website" />
-					<meta property="og:title" content={config.title} />
-					<meta property="og:description" content={config.description} />
-					<meta property="og:site_name" content={config.title} />
-					<meta property="og:image" content={config.image} />
-					<meta name="twitter:card" content="summary" />
-					<meta name="twitter:site" content="@anozon" />
-					<meta name="theme-color" content="#eee" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1.0"
-					/>
-					<script
-						async
-						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-					/>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
+export default function Document({ Main, Head, NextScript }: DocumentProps) {
+	return (
+		<html lang="ja">
+			<Head>
+				<title>{config.title}</title>
+				<link rel="shortcut icon" href="icon.png" />
+				<link rel="icon" type="image/png" href="icon.png" sizes="192x192" />
+
+				<meta name="description" content={config.description} />
+				<meta property="og:url" content={config.url} />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={config.title} />
+				<meta property="og:description" content={config.description} />
+				<meta property="og:site_name" content={config.title} />
+				<meta property="og:image" content={config.image} />
+				<meta name="twitter:card" content="summary" />
+				<meta name="twitter:site" content="@anozon" />
+				<meta name="theme-color" content="#eee" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -48,14 +49,13 @@ export default class extends Document {
               page_path: window.location.pathname,
             });
           `,
-						}}
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</html>
-		)
-	}
+					}}
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</html>
+	)
 }
