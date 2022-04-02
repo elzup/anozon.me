b0784465658be9d390de8ce98cec2525dcbe13a3
elzup <guild0105@gmail.com>
2022-04-03 00:59:11 +0900

    remove webpack

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-const webpack = require('webpack')
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 })
 
-const dist = __dirname + '/dist'
-
-const config = {
-	webpack: (config) => {
-		config.plugins.push(
-			new WorkboxWebpackPlugin.GenerateSW({
-				maximumFileSizeToCacheInBytes: 1024 * 1024 * 10,
-				swDest: dist + '/sw.js',
-				clientsClaim: true,
-				skipWaiting: true,
-			})
-		)
-
-		return config
-	},
-}
+const config = {}
 
 module.exports = withBundleAnalyzer(config)
