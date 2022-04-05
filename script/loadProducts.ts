9fccb2123dbf4eb635313d387eab97a97ebf6365
elzup <guild0105@gmail.com>
2022-04-06 00:28:52 +0900

    add notion script

---
+++ script/loadProducts.ts
@@ -0,0 +1,18 @@
+import { Client } from '@notionhq/client'
+import assert from 'assert'
+const { NOTION_SECRET, NOTION_TARGET_DB } = process.env
+
+assert(NOTION_SECRET, 'NOTION_SECRET not setup')
+assert(NOTION_TARGET_DB, 'NOTION_TARGET_DB not setup')
+
+const dbId = NOTION_TARGET_DB
+
+async function main() {
+	if (!dbId || NOTION_SECRET) return console.error
+
+	const notion = new Client({ auth: NOTION_SECRET })
+	const db = await notion.databases.query({ database_id: dbId })
+	console.log(db.results)
+}
+
+export default {}
